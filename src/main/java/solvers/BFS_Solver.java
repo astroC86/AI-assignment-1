2bedb2199dddab9d11ba0220272dc9d768855299
hassaniko <elsawyhassan2@gmail.com>
2021-11-06 22:36:26 +0200

    #1st commit (shabah zizo)

---
+++ src/main/java/solvers/BFS_Solver.java
@@ -0,0 +1,98 @@
+package solvers;
+
+import model.EightPuzzle;
+import org.xml.sax.SAXNotRecognizedException;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Queue;
+import java.util.Stack;
+
+public class BFS_Solver{
+    private class SearchNode {
+        private final int   moves;
+        private final EightPuzzle board;
+        private SearchNode  previous;
+
+        public SearchNode(EightPuzzle bd) {
+            if (bd == null) throw new IllegalArgumentException();
+            this.board    = bd;
+            this.moves    = 0;
+        }
+
+        public SearchNode(EightPuzzle board, SearchNode prev) {
+            if (board == null) throw new IllegalArgumentException();
+            if (prev == null) throw new IllegalArgumentException();
+            this.board     = board;
+            this.previous  = prev;
+            this.moves     = prev.moves + 1;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            SearchNode that = (SearchNode) o;
+            return board.equals(that.board);
+        }
+    }
+
+    private SearchNode current;
+
+
+    public BFS_Solver(EightPuzzle initialState){
+
+        if (initialState == null)
+            throw new IllegalArgumentException();
+
+        current = new SearchNode(initialState);
+
+        if(!current.board.isSolvable()) return;
+
+        Stack<SearchNode> frontier = new Stack<>();
+        Set<EightPuzzle> frontierSet = new HashSet<>();
+        Set<EightPuzzle> explored  =  new HashSet<>();
+
+        frontier.add(current);
+        frontierSet.add(current.board);
+
+        while(!frontier.isEmpty()){
+            current = frontier.remove();
+
+            explored.add(current.board);
+            frontierSet.remove(current.board);
+
+            if (current.board.isGoalState()) break;
+            for (var nb : current.board.getNeighbours()) {
+                if (current.previous == null || !nb.equals(current.previous.board)) {
+                    if(!explored.contains(nb) &&  !frontierSet.contains(nb)){
+                        frontier.add(new SearchNode(nb, current));
+                        frontierSet.add(nb);
+                    }
+                }
+            }
+        }
+
+    }
+    public int moves() {
+        if (!isSolvable()) return -1;
+        return this.current.moves;
+    }
+
+    public boolean isSolvable() {
+        return current.board.isSolvable();
+    }
+
+    public Iterable<EightPuzzle> solution() {
+        if (!isSolvable())
+            return null;
+        Stack<EightPuzzle> seq = new Stack<>();
+        SearchNode runner = current;
+        while (runner != null) {
+            seq.push(runner.board);
+            runner = runner.previous;
+        }
+        return seq;
+    }
+}
