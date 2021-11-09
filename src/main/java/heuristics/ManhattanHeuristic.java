e286c0e39fc255635ee278fc729f437f4376b0fa
hassaniko <elsawyhassan2@gmail.com>
2021-11-09 12:12:51 +0200

    initial push ManHeuristic

--- src/main/java/heuristics/ManhattanHeuristic.java
+++ src/main/java/heuristics/ManhattanHeuristic.java
@@ -1,11 +1,32 @@
 package heuristics;
 
 import model.EightPuzzle;
+import java.lang.Math;
 
 public final class ManhattanHeuristic implements Heuristic {
     @Override
     public double evaluate(EightPuzzle state) {
         // TODO: Implement
-        return 0;
+        int sumMH = 0 ;// heuristic
+        int currPos, targetPos, currR, currC, targetR, targetC; // stands for row and column
+        int[] boardCoord;
+        for(int i = 0; i < 9 ; i++){
+
+            currPos = i;
+            targetPos = state.getNumberAtIndex(i);
+
+            boardCoord = state.arrayCoordinateToBoardCoordinates(currPos);
+            currR = boardCoord[0] ;
+            currC = boardCoord[1] ;
+
+            boardCoord = state.arrayCoordinateToBoardCoordinates(targetPos);
+            targetR = boardCoord[0] ;
+            targetC = boardCoord[1] ;
+
+            sumMH += Math.abs(targetR - currR) + Math.abs(targetC - currC);
+
+        }
+
+        return sumMH;
     }
 }
